49a08c23779870c016abb9d7344206215ce1ec32
Pavel Mezencev <pamezencev@gmail.com>
2021-08-28 20:58:53 +0300

    add stycky nav and smooth scrolling

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.querySelector(".btn-mobile-nav");
-const header = document.querySelector("header");
-
-btnMenu.addEventListener("click", function () {
-  header.classList.toggle("nav-open");
-});
---
+++ js/script.js
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////
+///////// MENU BUTTON
+
+const btnMenu = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector("header");
+
+btnMenu.addEventListener("click", function () {
+  closeMobMenu();
+});
+
+function closeMobMenu() {
+  header.classList.toggle("nav-open");
+}
+/////////////////////////////////////////////
+///////// CURRENT YEAR
+
+const yearEl = document.querySelector(".year");
+yearEl.textContent = new Date().getFullYear();
+
+/////////////////////////////////////////////
+///////// SMOOTH SCROLLING ANIMATION
+
+const links = document.querySelectorAll("a:link");
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // goes up
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    const el = document.querySelector(href);
+    // goes to section
+    if (href !== "#" && href.startsWith("#")) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+    // close mobile menu
+    if (e.target.classList.contains("header-menu-item")) closeMobMenu();
+  });
+});
+
+/////////////////////////////////////////////
+///////// STICKY NAVIGATION
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const [ent] = entries;
+    if (!ent.isIntersecting) {
+      document.querySelector(".header").classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.querySelector(".header").classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-126px",
+    // mean no el in the viewport, 0%. If 1, mean all el inside the viewport
+  }
+);
+observer.observe(sectionHeroEl);
